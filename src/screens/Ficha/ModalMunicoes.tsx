5f69376e17cc2bbc9c7a8cc294429b4cb3293b1f
quackthalia <naninathbruch@gmail.com>
2026-08-26 09:31:34 -0300

    Increase description preview height from 36px to 54px for all Item Modals except Weapons

--- src/screens/Ficha/ModalMunicoes.tsx
+++ src/screens/Ficha/ModalMunicoes.tsx
@@ -148,7 +148,7 @@ export function ModalMunicoes({ onFechar, armaFiltroNome, armaFiltroCategoria, o
                         <span className="italic text-zinc-400">{municao.Tipo_Arma}</span>
                       </div>
                       
-                      <p className="text-xs text-zinc-400 mb-4 leading-relaxed whitespace-pre-wrap select-none min-h-[36px]">
+                      <p className="text-xs text-zinc-400 mb-4 leading-relaxed whitespace-pre-wrap select-none min-h-[54px]">
                         {formatarTexto(municao.Descricao_Item)}
                       </p>
                     </div>
@@ -196,7 +196,7 @@ export function ModalMunicoes({ onFechar, armaFiltroNome, armaFiltroCategoria, o
                         <span className="italic text-zinc-400">{municao.Tipo_Arma}</span>
                       </div>
                       
-                      <p className="text-xs text-zinc-400 mb-4 leading-relaxed whitespace-pre-wrap select-none min-h-[36px]">
+                      <p className="text-xs text-zinc-400 mb-4 leading-relaxed whitespace-pre-wrap select-none min-h-[54px]">
                         {formatarTexto(municao.Descricao_Item)}
                       </p>
                     </div>
